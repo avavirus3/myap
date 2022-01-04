e6378237d9ebba8690d48db6b4521f18d004f0a8
avavirus3 <49475731+avavirus3@users.noreply.github.com>
2022-01-04 16:05:28 +0300

    react basics first time

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import Loading from "./Loading";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      loaded: true,
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  getUsers() {
+    this.setState({
+      loaded: false,
+    });
+    axios("https://random-data-api.com/api/users/random_user?size=5").then(
+      (response) =>
+        this.setState({
+          users: [...this.state.users, ...response.data],
+          loaded: true,
+        })
+    );
+  }
+  handleSubmit() {
+    this.getUsers();
+    console.log("pick a pick");
+  }
+  componentDidMount() {
+    this.getUsers();
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.loaded ? (
+          this.state.users.map((user) => (
+            <div key={user.id}>
+              <h1>{user.first_name}</h1>
+              <h3>{user.email}</h3>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+              <hr />
+            </div>
+          ))
+        ) : (
+          <Loading message="Wait it's" />
+        )}
+        <form>
+          <input type="button" onClick={this.handleSubmit} value="load users" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Loading.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const Loading = ({message}) => <h2> {message} Loading</h2>
+export default Loading;
